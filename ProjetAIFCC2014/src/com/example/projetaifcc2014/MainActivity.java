59ec43bd4bd8202b7f6560ae4fa1b0172e62ff9b
florian_r <florian.renee@live.fr>
2014-03-29 17:22:03 +0100

    Page de choix des profils

--- ProjetAIFCC2014/src/com/example/projetaifcc2014/MainActivity.java
+++ ProjetAIFCC2014/src/com/example/projetaifcc2014/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.projetaifcc2014;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	private int profil = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.profil);
+	}
+	
+	public void onClick(View v) {
+		switch(v.getId()) {
+			case R.id.profil_1 : profil = 1; break;
+			case R.id.profil_2 : profil = 2; break;
+			case R.id.profil_3 : profil = 3; break;
+			case R.id.profil_4 : profil = 4; break;
+			case R.id.profil_5 : profil = 5; break;
+    		case R.id.profil_6 : profil = 6; break;
+    	}
+		setContentView(R.layout.accueil);
 	}
 
 	@Override
