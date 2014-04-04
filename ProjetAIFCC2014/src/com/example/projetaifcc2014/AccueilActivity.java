305b6f2e5a60f2b72c635a8d84954d64e43e30bc
jerome <jerome.hubner@gmail.com>
2014-04-04 23:28:21 +0200

    Jerome : 2

---
+++ ProjetAIFCC2014/src/com/example/projetaifcc2014/AccueilActivity.java
@@ -0,0 +1,16 @@
+package com.example.projetaifcc2014;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AccueilActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.accueil);
+	}
+
+	
+	
+}
---
+++ ProjetAIFCC2014/src/com/example/projetaifcc2014/ProfilActivity.java
@@ -0,0 +1,55 @@
+package com.example.projetaifcc2014;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class ProfilActivity extends Activity {
+
+	private int profil = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profil);
+	}
+
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.profil_1 :
+			profil = 1;
+			Toast.makeText(this, "test", 3000).show();
+			Intent intentAccueil = new Intent(getActivity(), AccueilActivity.class);
+			startActivity(intentAccueil);
+			
+			break;
+		
+		case R.id.profil_2 : profil = 2; break;
+		
+		case R.id.profil_3 : profil = 3; break;
+		
+		case R.id.profil_4 : profil = 4; break;
+		
+		case R.id.profil_5 : profil = 5; break;
+		
+		case R.id.profil_6 : profil = 6; break;
+		}
+	}
+
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public Activity getActivity(){
+		return this;
+	}
+	
+}
