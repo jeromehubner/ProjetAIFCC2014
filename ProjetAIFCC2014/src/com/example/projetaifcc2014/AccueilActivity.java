18f1a8466059d70f972c9c0ff6604fec17089cd2
Fabien <fournier.fabien14@gmail.com>
2014-04-18 14:34:04 +0200

    Ajout de la redirection google map

--- ProjetAIFCC2014/src/com/example/projetaifcc2014/AccueilActivity.java
+++ ProjetAIFCC2014/src/com/example/projetaifcc2014/AccueilActivity.java
@@ -1,9 +1,13 @@
 package com.example.projetaifcc2014;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AccueilActivity extends Activity {
 
@@ -23,6 +27,24 @@ public class AccueilActivity extends Activity {
 			((TextView)findViewById(R.id.titrePageAccueil)).setText(intent.getStringExtra(argTitrePageAccueil));
 			((TextView)findViewById(R.id.descriptionPageAccueil)).setText(intent.getStringExtra(argContDescr));
 			((TextView)findViewById(R.id.financementContenu)).setText(intent.getStringExtra(argContFin));
+		}
+	}
+	
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.plan_aifcc:
+			try {
+				Toast.makeText(this, "Redirection vers google maps", Toast.LENGTH_LONG);
+				
+				// Ici, dans le ?q, mettez ce que vous voulez trouver sur google map
+				Uri urimap = Uri.parse("geo:0,0?q=AIFCC Caen");
+				Intent mapIntent = new Intent(Intent.ACTION_VIEW,urimap);
+				startActivity(mapIntent); 
+			} catch(ActivityNotFoundException e) {
+				(Toast.makeText(this.getApplicationContext(), "GoogleMap non trouvé", Toast.LENGTH_LONG)).show();
+			}
+		
+		break;
 		}
 	}
 }
