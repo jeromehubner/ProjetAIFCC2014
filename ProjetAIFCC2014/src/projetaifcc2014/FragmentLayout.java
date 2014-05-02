1a8ac642e57a867771c8501406bef14e307df61c
florian_r <florian.renee@live.fr>
2014-05-02 17:27:15 +0200

    detail formation + fragment layout

---
+++ ProjetAIFCC2014/src/projetaifcc2014/FragmentLayout.java
@@ -0,0 +1,23 @@
+package projetaifcc2014;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+@SuppressLint("ValidFragment")
+public class FragmentLayout extends Fragment {
+	private int idLayout;
+
+	public FragmentLayout(){}
+	public FragmentLayout(int idLayout){
+		this.idLayout = idLayout;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(idLayout, container, false);
+	}
+}
