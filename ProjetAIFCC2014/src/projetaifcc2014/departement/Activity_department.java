7a68456eed0cfc565666a7fb3339507c9ff00d4f
Fabien <fournier.fabien14@gmail.com>
2014-05-05 20:54:20 +0200

    ajout des viewflippers pour la banniere et les notifications.
    Bien à vous, 
    
    FOURNIER Fabien

--- ProjetAIFCC2014/src/projetaifcc2014/departement/Activity_department.java
+++ ProjetAIFCC2014/src/projetaifcc2014/departement/Activity_department.java
@@ -7,7 +7,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import com.example.projetaifcc2014.R;
 
@@ -26,13 +33,63 @@ public class Activity_department extends Activity {
 	private String urlAIFCC = "http://www.aifcc.com/";
 	private String urlimss = "http://www.imss-caen.com/";
 	private String urlIMAD = "http://www.imad-caen.com/";
-
+	
+	ImageView banniere_element1;
+	ImageView banniere_element2;
+	ImageView banniere_element3;
+	ViewFlipper vfBan;
+	ViewFlipper vfActu;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.screen_department);
 		
+		// Le contenu suivant concerne le ViewFlipper
 		
+				// Instanciation des objets
+				banniere_element1 = (ImageView)findViewById(R.id.banniere_element1);
+				banniere_element2 = (ImageView)findViewById(R.id.banniere_element2);
+				banniere_element3 = (ImageView)findViewById(R.id.banniere_element3);
+				vfBan = (ViewFlipper)findViewById(R.id.ViewFlipperImages);
+				
+				// Définition des animations de slide
+				Animation right_in = AnimationUtils.loadAnimation(this.getApplicationContext(),R.anim.right_in);
+				Animation left_out = AnimationUtils.loadAnimation(this.getApplicationContext(),R.anim.left_out);
+				
+				Animation fadeIn = new AlphaAnimation(0, 1);
+				fadeIn.setInterpolator(new DecelerateInterpolator());
+				fadeIn.setDuration(1000);
+				
+				Animation fadeOut = new AlphaAnimation(1, 0);
+				fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+				fadeOut.setStartOffset(1000);
+				fadeOut.setDuration(1000);
+				
+				Animation fadeIn2 = new AlphaAnimation(0, 1);
+				fadeIn.setInterpolator(new DecelerateInterpolator());
+				fadeIn.setDuration(1000);
+				
+				Animation fadeOut2 = new AlphaAnimation(1, 0);
+				fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+				fadeOut.setStartOffset(1000);
+				fadeOut.setDuration(1000);
+				
+				// Affectation des animations
+				vfBan.setInAnimation(fadeIn);
+				vfBan.setOutAnimation(fadeOut);
+				
+				// Paramètrage du comportement du ViewFlipper banniere
+				vfBan.setAutoStart(true);
+				vfBan.startFlipping();
+				
+				// Paramètrage du ViewFlipper actualités
+				vfActu = (ViewFlipper)findViewById(R.id.ViewFlipperActus);
+				vfActu.setInAnimation(right_in);
+				vfActu.setOutAnimation(fadeOut2);
+				
+				vfActu.setAutoStart(true);
+				vfActu.startFlipping();
 //		// cette ligne indique que lorsque l'on clique sur la notification, nous revenons vers notre application
 //		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, orgActivity.class), 0);
 //				
@@ -120,7 +177,7 @@ public class Activity_department extends Activity {
 			break;
 
 			// Bouton de banniere qui ouvre le site //
-		case R.id.banniere:
+		case R.id.banniere_element1:
 
 			Uri uriAIFCC = Uri.parse(urlAIFCC);
 			Toast.makeText(this, "Redirection vers" + urlAIFCC, Toast.LENGTH_LONG).show();
