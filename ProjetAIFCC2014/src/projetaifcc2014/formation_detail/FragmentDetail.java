201cfc26fd2300aef979dbfdfc91466da51ee5d3
florian_r <florian.renee@live.fr>
2014-06-23 14:23:35 +0200

    Flo R : 23/06 - Page contact + Héritage FragmentDetail

--- ProjetAIFCC2014/src/projetaifcc2014/formation_detail/FragmentDetail.java
+++ ProjetAIFCC2014/src/projetaifcc2014/formation_detail/FragmentDetail.java
@@ -4,6 +4,7 @@ import projetaifcc2014.database.formation.Formation;
 import projetaifcc2014.database.formation.FormationBdd;
 import projetaifcc2014.drawer.Activity_drawer;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +13,15 @@ import android.webkit.WebView;
 import com.example.projetaifcc2014.R;
 
 
-public class FragmentDetail1 extends FragmentDetail{
+public class FragmentDetail extends Fragment implements InterfaceFragmentDetail{
 
-	private int idFormation;
-	private String contentMetiersVises;
-	private final String contentEmpty = "Pas d'informations disponibles pour le moment...";
+	protected int idFormation;
+	protected String emailFormation;
+	protected String contentMetiersVises;
+	protected String contentModalités;
+	protected String contentProgramme;
+	protected final String contentEmpty = "Pas d'informations disponibles pour le moment...";
+	protected String couleurDepartement;
 
 
 	@Override
@@ -50,5 +55,36 @@ public class FragmentDetail1 extends FragmentDetail{
 		metiersVises.loadDataWithBaseURL(null, contentMetiersVises, "text/html","utf-8", null);
 		
 		return view;
+	}
+	
+	@Override
+	public String htmlContentInit(Formation formation)
+	{
+		String tagDebut = "<font color="+couleurDepartement+">", tagFin = "</font>";
+		String htmlString = formation.getProgramme();
+		
+		String temp = htmlString.replaceAll("<h2>", "<h3>");
+
+		htmlString = temp.replaceAll("<ul>", "<!--");
+		temp = htmlString.replaceAll("</ul>", "-->");
+
+		if(temp.equals(""))
+			return contentEmpty;
+		else 
+			return tagDebut+temp+tagFin;
+	}
+
+
+	@Override
+	public String getHTMLCouleurDepartement(Formation formation)
+	{
+		int indexArrayColor = formation.getCategorie().getDepartement().getId() -1;
+
+		// On récupère le tableau des couleurs de départements renseignés dans le fichier color.xml
+		int[] colorArray = getResources().getIntArray(R.array.dpt_color);
+
+		String couleurTemp = Integer.toHexString(colorArray[indexArrayColor]);
+
+		return couleurTemp.replace("ff", "#");
 	}
 }
--- ProjetAIFCC2014/src/projetaifcc2014/formation_detail/FragmentDetail4.java
+++ ProjetAIFCC2014/src/projetaifcc2014/formation_detail/FragmentDetail4.java
@@ -1,20 +1,74 @@
 package projetaifcc2014.formation_detail;
 
+import projetaifcc2014.database.formation.Formation;
+import projetaifcc2014.database.formation.FormationBdd;
+import projetaifcc2014.drawer.Activity_drawer;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.projetaifcc2014.R;
 
-
-public class FragmentDetail4 extends Fragment{
+public class FragmentDetail4 extends FragmentDetail{
+	
+	Button btnEnvoi;
+	EditText mailSujet;
+	EditText mailMessage;
 	
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_formation_detail_4, container, false);
+		
+		FormationBdd formationBdd = new FormationBdd(getActivity());
+
+		formationBdd.open();			
+		Formation formation = formationBdd.getFormationById(((Activity_drawer) getActivity()).getIdFormation());
+		formationBdd.close();
+		
+		couleurDepartement = getHTMLCouleurDepartement(formation);
+		emailFormation = formation.getEmail();
+
+		Integer couleur = Color.parseColor(couleurDepartement);
+		TextView textView = (TextView) view.findViewById(R.id.mail_sujet);
+		textView.setTextColor(couleur);
+		textView = (TextView) view.findViewById(R.id.mail_message);
+		textView.setTextColor(couleur);
+		
+		btnEnvoi = (Button) view.findViewById(R.id.btn_envoi);
+		mailSujet = (EditText) view.findViewById(R.id.mail_sujet_edit);
+		mailMessage = (EditText) view.findViewById(R.id.mail_message_edit);
+
+		if(emailFormation != null && !emailFormation.equals("")){
+			btnEnvoi.setOnClickListener(new OnClickListener() {
+	
+				@Override
+				public void onClick(View v) {
+					
+					String subject = mailSujet.getText().toString();
+					String message = mailMessage.getText().toString();
+	
+					Intent email = new Intent(Intent.ACTION_SEND);
+					email.putExtra(Intent.EXTRA_EMAIL, new String[]{emailFormation});
+					email.putExtra(Intent.EXTRA_SUBJECT, subject);
+					email.putExtra(Intent.EXTRA_TEXT, message);
+					email.setType("message/rfc822");
+	
+					startActivity(Intent.createChooser(email, "Choisir un client Email :"));
+				}
+			});
+		} else {
+			btnEnvoi.setEnabled(false);
+			btnEnvoi.setText("Auncun contact");
+		}
+
 		return view;
 	}
 }
+
