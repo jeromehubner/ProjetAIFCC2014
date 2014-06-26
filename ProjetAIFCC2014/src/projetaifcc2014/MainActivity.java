d8bb2c2ef1ef2d5c527ae7210c0e63df1a7bf879
jerome <jerome@jerome-ThinkPad-Edge>
2014-06-26 15:41:30 +0200

    Jerome : Implementation de la progressBar dynamique du SplashScreen

--- ProjetAIFCC2014/src/projetaifcc2014/MainActivity.java
+++ ProjetAIFCC2014/src/projetaifcc2014/MainActivity.java
@@ -7,17 +7,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.ProgressBar;
 
 import com.example.projetaifcc2014.R;
 
 public class MainActivity extends Activity {
 	private Thread splashTread;
 	private Activity mainActivity;
+	private ProgressBar progressBar;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.screen_splash);
+		progressBar = (ProgressBar) findViewById(R.id.progressBar1);
 		mainActivity = this;
 
 		// Thread pour l'affichage du SplashScreen
@@ -25,29 +29,77 @@ public class MainActivity extends Activity {
 			@Override
 			public void run() {
 				try {
-					synchronized(this){
-						wait();					
+					synchronized (this) {
+						wait();
 					}
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				} finally {
 					finish();
-					Intent i = new Intent(getBaseContext(), Activity_drawer_Departement.class);
+					Intent i = new Intent(getBaseContext(),
+							Activity_drawer_Departement.class);
 					startActivity(i);
 				}
 			}
 		};
 		splashTread.start();
 
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		new Thread() {
+			@Override
+			public void run() {
+				int facteur = 4; // facteur utilisé pour le calcul du temps dans
+									// les méthodes Thread.sleep(time);
+				int vitesseBalayage = 25; // Coefficient inverse de la vitesse
+				int nbSecondesBalayage = 60; // Durée de vie du thread en
+												// secondes
+				if (progressBar.getProgress() <= 99) {
+					for (int i = 0; i < (nbSecondesBalayage * facteur); i++) {
+
+						progressBar.setSecondaryProgress(progressBar
+								.getSecondaryProgress() + 2);
+
+						Log.v("mytag",
+								"SecondaryProgressBar : "
+										+ progressBar.getSecondaryProgress());
+						Log.v("mytag",
+								"progressBar : " + progressBar.getProgress());
+
+						for (int j = 0; j < progressBar.getSecondaryProgress(); j++) {
+
+							progressBar.setProgress(j);
+							try {
+								// La vitesse de parcours de la progressBar
+								Thread.sleep(vitesseBalayage);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+
+						try {
+							// Le temps que la progressBar attends à la fin du
+							// chargement
+							Thread.sleep(1000 / facteur);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+				Log.v("mytag", "Fermeture du thread");
+			}
+		}.start();
+
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
 		if (!prefs.getBoolean("firstTime", false)) {
 			SharedPreferences.Editor editor = prefs.edit();
 			editor.putBoolean("firstTime", true);
 			editor.commit();
-			new InitDatabase(mainActivity, splashTread).execute();
+			new InitDatabase(mainActivity, splashTread, progressBar).execute();
 		} else {
-			synchronized(splashTread){
-				splashTread.notifyAll();					
+			synchronized (splashTread) {
+				splashTread.notifyAll();
 			}
 		}
 	}
--- ProjetAIFCC2014/src/projetaifcc2014/database/InitDatabase.java
+++ ProjetAIFCC2014/src/projetaifcc2014/database/InitDatabase.java
@@ -6,33 +6,41 @@ import projetaifcc2014.database.handler.TableFormationHandler;
 import projetaifcc2014.database.handler.TableFormationToCategorieHandler;
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ProgressBar;
 
 public class InitDatabase extends AsyncTask<Void, Void, Void>{
 
 	private Activity mainActivity;
 	private Thread splashThread;
+	private ProgressBar progressBar;
 
-	public InitDatabase (Activity mainActivity, Thread splashThread){
+	public InitDatabase (Activity mainActivity, Thread splashThread, ProgressBar progressBar){
 		this.mainActivity = mainActivity;
 		this.splashThread = splashThread;
+		this.progressBar = progressBar;
 	}
 	
 
+	
 	@Override
 	protected Void doInBackground(Void... params) {
 
 		TableCategorieToCategorieHandler tableCatToCatHandler = new TableCategorieToCategorieHandler(mainActivity);
 		tableCatToCatHandler.processFeed();
+		progressBar.setSecondaryProgress(20);
 
 		TableCategorieHandler tableCategorieHandler = new TableCategorieHandler(mainActivity);
 		tableCategorieHandler.processFeed();
-
+		progressBar.setSecondaryProgress(40);
+		
 		TableFormationToCategorieHandler tableFormToCatHandler = new TableFormationToCategorieHandler(mainActivity);
 		tableFormToCatHandler.processFeed();
-
+		progressBar.setSecondaryProgress(60);
+		
 		TableFormationHandler tableFormationHandler = new TableFormationHandler(mainActivity);
 		tableFormationHandler.processFeed();
-
+		progressBar.setSecondaryProgress(100);
 		return null;
 	}
 
